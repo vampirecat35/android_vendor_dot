2093336b1798d6dac88a2ddef3a44fa4eb4d673b
Andy Vandijck <andyvand@gmail.com>
2023-05-29 21:17:52 +0200

    Update BoardConfigQcom.mk

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -7,6 +7,7 @@ MSMSTEPPE ?= sm6150
 TRINKET ?= trinket #SM6125
 ATOLL ?= atoll #SM6250
 LAHAINA ?= lahaina #SM8350
+LEMON ?= lemon #SM6115
 
 B_FAMILY := msm8226 msm8610 msm8974
 B64_FAMILY := msm8992 msm8994
